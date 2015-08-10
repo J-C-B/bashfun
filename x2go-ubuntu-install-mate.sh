558d5620d6b0bc450388ae053dcc2464633d7c67
John <john@Johns-MacBook-Pro.local>
2015-08-11 09:19:36 +1200

    Added x2go Script

---
+++ x2go-ubuntu-install-mate.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+####################################################################################
+####################        Install X2Go         ###################################
+####################        Pulled from          ###################################
+####### http://www.jupiterbroadcasting.com/85377/get-going-with-x2go-las-374/ ######
+####################        John 11/08/15        ###################################
+####################################################################################
+
+sudo apt-get update && apt-get install python-software-properties -y && add-apt-repository ppa:x2go/stable -y
+
+sudo apt-get update -y && apt-get install x2goserver x2goserver-xsession -y
+
+####################################################################################
+#### Use these for ubuntu server (ie previously headless machine)
+####################################################################################
+
+#sudo apt-add-repository ppa:ubuntu-mate-dev/ppa -y
+
+#sudo apt-add-repository ppa:ubuntu-mate-dev/trusty-mate -y
+
+#sudo apt-get update && sudo apt-get upgrade -y
+
+#sudo apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop
+
+####################################################################################
+## Include these if you also wish to contol other machines remotley from this host##
+####################################################################################
+
+#sudo apt-get update
+
+#sudo apt-get install x2goclient
